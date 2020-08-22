1f90c6b2291430f53b5270b7057adf6a6e121f3e
afoyer <lemimic213@gmail.com>
2020-08-22 18:14:41 -0500

    Added Photo Gallery Page

--- src/components/flickr.js
+++ src/components/flickr.js
@@ -1,17 +1,35 @@
 import React from "react";
+import { motion } from "framer-motion";
 
-function Flikr() {
+function Flikr(props) {
   const api_key = process.env.REACT_APP_API_KEY;
   const user_id = process.env.REACT_APP_USER_ID;
+
+  // const [ref, inView] = useInView({ threshold: 0.6, triggerOnce: true });
   return fetch(
-    `https://www.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=${api_key}&user_id=${user_id}`
+    `https://www.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=${api_key}&user_id=${user_id}&format=json&nojsoncallback=?&extras=url,l`
   )
     .then((response) => {
       return response.json();
     })
     .then((jsonResponse) => {
+      // console.log(jsonResponse);
       return jsonResponse.photos.photo.map((pic) => {
-        return `https://farm${pic.farm}.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}.jpg`;
+        var srcPath = `https://farm${pic.farm}.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}.jpg`;
+        return (
+          <a href={`https://www.flickr.com/photos/aymericf/${pic.id}`}>
+            <motion.img
+              ref={props.ref}
+              key={pic.id}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              className="flickrImage"
+              src={srcPath}
+            />
+          </a>
+        );
       });
     });
 }
+export default Flikr;
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,15 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
+import Flickr from "../components/flickr";
+import { useInView } from "react-intersection-observer";
 
 function Projects() {
+  const [photos, storePhotos] = useState();
+  const [loading, setLoading] = useState(false);
+  // const [background, placeBackground] = useState();
+
+  // const setBackground = (image)=>{
+
+  // }
+  const [ref, inView] = useInView({ threshold: 0.6, triggerOnce: true });
+  useEffect(() => {
+    Flickr(ref, inView).then((array) => {
+      storePhotos(array);
+      setLoading(true);
+    });
+  }, []);
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-    >
-      hey
-    </motion.div>
+    <>
+      <div className="loader">
+        {!loading && (
+          <motion.img
+            key="load"
+            className="load"
+            initial={false}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            src="https://raw.githubusercontent.com/afoyer/portfolio/master/public/logo512.png"
+          />
+        )}
+      </div>
+      <motion.div className="imageContainer">{loading && photos}</motion.div>
+    </>
   );
 }
 export default Projects;
