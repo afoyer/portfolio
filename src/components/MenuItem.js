b0bbcd32387f091c89b67cb4be4280d1da61133b
afoyer <lemimic213@gmail.com>
2020-08-23 22:11:36 -0500

    Updated Header

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -4,33 +4,32 @@ import { Link } from "react-router-dom";
 
 //MenuItem is just an Anchor animated tag. Can set href and name using props
 
-//Framer Motion Variants. May remove if I want to make it a prop
-const itemVariants = {
-  open: {
-    x: 0,
-    y: 0,
-    opacity: 1,
-    borderRadius: "6px",
-    color: "#FFFFFF",
-    transition: {
-      duration: 0.5,
-    },
-  },
-  collapsed: {
-    opacity: 0,
-
-    color: "#212121",
-    transition: { duration: 0.2 },
-  },
-};
-
 function MenuItem(props) {
+  //Framer Motion Variants. May remove if I want to make it a prop
+  const itemVariants = {
+    open: {
+      x: 0,
+      y: 0,
+      opacity: 1,
+      borderRadius: "6px",
+      color: "#FFFFFF",
+      transition: {
+        duration: 0.5,
+      },
+    },
+    collapsed: {
+      opacity: 1,
+
+      color: "#212121",
+      transition: { duration: 0.2 },
+    },
+  };
   return (
     //This div is just so I can scale on hover...
 
     <motion.div whileHover={{ scale: 1.05 }}>
       <Link to={props.link} style={{ textDecoration: "none" }}>
-        <motion.p
+        <motion.h3
           whileHover={{
             color: "#212121",
             backgroundColor: props.backgroundSelectColor,
@@ -48,7 +47,7 @@ function MenuItem(props) {
           className="menuItem"
         >
           {props.name}
-        </motion.p>
+        </motion.h3>
       </Link>
     </motion.div>
   );
