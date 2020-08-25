9dab97a1916b3e35543ecc8173c78090cb93f4d3
afoyer <lemimic213@gmail.com>
2020-08-25 13:59:00 -0500

    added close button for clearer UI

--- src/components/projectItem.js
+++ src/components/projectItem.js
@@ -58,7 +58,12 @@ function CompactProjectCard({
 function ExpandedProjectCard({ children, data, onCollapse }) {
   return (
     <>
-      <svg
+      <motion.svg
+        initial={{ opacity: 0 }}
+        animate={{
+          opacity: 1,
+          transition: { delay: 0.5, duration: 0.5 },
+        }}
         onClick={onCollapse}
         className="close-button"
         xmlns="http://www.w3.org/2000/svg"
@@ -68,7 +73,7 @@ function ExpandedProjectCard({ children, data, onCollapse }) {
       >
         <path d="M0 0h24v24H0V0z" fill="none" />
         <path d="M18.3 5.71c-.39-.39-1.02-.39-1.41 0L12 10.59 7.11 5.7c-.39-.39-1.02-.39-1.41 0-.39.39-.39 1.02 0 1.41L10.59 12 5.7 16.89c-.39.39-.39 1.02 0 1.41.39.39 1.02.39 1.41 0L12 13.41l4.89 4.89c.39.39 1.02.39 1.41 0 .39-.39.39-1.02 0-1.41L13.41 12l4.89-4.89c.38-.38.38-1.02 0-1.4z" />
-      </svg>
+      </motion.svg>
       <motion.div
         // animate={{ scale: 1 }}
         className="card expanded"
