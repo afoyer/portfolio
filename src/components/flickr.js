b4bfdab5feb61f7859c6834b76458fb784b60662
afoyer <lemimic213@gmail.com>
2020-09-10 11:25:42 -0500

    Infinite scroll setup

--- src/components/flickr.js
+++ src/components/flickr.js
@@ -1,41 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { motion } from "framer-motion";
 
 /**
  * Flickr fetches pictures from *my* profile and creates an <img> array
- * with links to "every" image to display (100) with clickable links
+ * with links to images to display (currently 15 per page) with clickable links
  */
-function Flikr() {
+function Flikr(pagenumber, imagecount) {
   // :)
   const api_key = process.env.REACT_APP_API_KEY;
   const user_id = process.env.REACT_APP_USER_ID;
+
   return fetch(
-    `https://www.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=${api_key}&user_id=${user_id}&format=json&nojsoncallback=?&extras=url,l`
+    `https://www.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=${api_key}&user_id=${user_id}&format=json&nojsoncallback=?&per_page=${imagecount}&page=${pagenumber}`
   )
     .then((response) => {
       return response.json();
     })
     .then((jsonResponse) => {
-      //Turn json into actual image links
+      //Turn json into objects with link and id for anchor
       return jsonResponse.photos.photo.map((pic) => {
         var srcPath = `https://farm${pic.farm}.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}.jpg`;
-        return (
-          <a
-            rel="noreferrer noopener"
-            key={`imageAnchor${pic.id}`}
-            href={`https://www.flickr.com/photos/aymericf/${pic.id}`}
-            target="_blank"
-          >
-            <motion.img
-              key={pic.id}
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              className="flickrImage"
-              src={srcPath}
-            />
-          </a>
-        );
+        return { path: srcPath, id: pic.id };
       });
     });
 }
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -3,17 +3,31 @@ import { motion } from "framer-motion";
 import Flickr from "../components/flickr";
 
 function Projects() {
-  const [photos, storePhotos] = useState();
+  const imageperpage = 15;
+  const [photos, storePhotos] = useState([]);
   const [loading, setLoading] = useState(false);
-
+  const [pagenumber, setPage] = useState(1);
+  const [hasMore, isHasMore] = useState(true);
   //Fetch images to display, unset loading state
   useEffect(() => {
-    Flickr().then((array) => {
-      storePhotos(array);
-      setLoading(true);
-    });
-  }, []);
+    //If all images haven't been loaded yet, go and fetch images
+    if (hasMore) {
+      setLoading(false);
 
+      Flickr(pagenumber, imageperpage).then((array) => {
+        //append new image links to previous ones
+        storePhotos((photos) => {
+          return [...photos, ...array];
+        });
+        //Check if this was last set of images
+        isHasMore(array.length >= imageperpage);
+        setLoading(true);
+      });
+    }
+  }, [pagenumber]);
+  function increase() {
+    setPage((page) => page + 1);
+  }
   return (
     <motion.div
       initial={{ opacity: 0 }}
@@ -21,6 +35,94 @@ function Projects() {
       exit={{ opacity: 0 }}
       className="photos"
     >
+      {/* IMAGES */}
+
+      <motion.div className="photoBanner">
+        <motion.h1>
+          A small collection of my photos. <br /> Follow me on{" "}
+          {/* Link to Flickr Account */}
+          <span>
+            <motion.a
+              className="flickr-anchor"
+              initial={false}
+              animate={{
+                background:
+                  "linear-gradient(98deg, rgba(82,160,218,1) 0%, rgba(82,160,218,1) 50%, rgba(82,160,218,1) 100%)",
+                WebkitBackgroundClip: "text",
+                WebkitTextFillColor: "transparent",
+              }}
+              href="https://www.flickr.com/photos/aymericf/"
+              rel="noreferrer noopener"
+              target="_blank"
+              whileHover={{
+                background: [
+                  "linear-gradient(98deg, rgba(252,176,69,1) 0%, rgba(131,58,180,1) 50%, rgba(253,29,29,1) 100%)",
+                  "linear-gradient(98deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%)",
+
+                  "linear-gradient(98deg, rgba(253,29,29,1) 0%, rgba(252,176,69,1) 50%, rgba(131,58,180,1) 100%)",
+
+                  "linear-gradient(98deg, rgba(252,176,69,1) 0%, rgba(131,58,180,1) 50%, rgba(253,29,29,1) 100%)",
+                ],
+                transition: { loop: Infinity, duration: 2, type: "spring" },
+              }}
+            >
+              Flickr
+            </motion.a>
+          </span>{" "}
+          or{" "}
+          <span>
+            <motion.a
+              className="flickr-anchor"
+              initial={false}
+              animate={{
+                background:
+                  "linear-gradient(98deg, rgba(82,160,218,1) 0%, rgba(82,160,218,1) 50%, rgba(82,160,218,1) 100%)",
+                WebkitBackgroundClip: "text",
+                WebkitTextFillColor: "transparent",
+              }}
+              href="https://www.instagram.com/aymericjlf/"
+              rel="noreferrer noopener"
+              target="_blank"
+              whileHover={{
+                background: [
+                  "linear-gradient(98deg, rgba(252,176,69,1) 0%, rgba(131,58,180,1) 50%, rgba(253,29,29,1) 100%)",
+                  "linear-gradient(98deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%)",
+
+                  "linear-gradient(98deg, rgba(253,29,29,1) 0%, rgba(252,176,69,1) 50%, rgba(131,58,180,1) 100%)",
+
+                  "linear-gradient(98deg, rgba(252,176,69,1) 0%, rgba(131,58,180,1) 50%, rgba(253,29,29,1) 100%)",
+                ],
+                transition: { loop: Infinity, duration: 2, type: "spring" },
+              }}
+            >
+              Instagram
+            </motion.a>
+          </span>
+          .
+        </motion.h1>
+      </motion.div>
+
+      <motion.div className="imageContainer">
+        {photos.map((photo) => {
+          return (
+            <a
+              rel="noreferrer noopener"
+              key={`imageAnchor${photo.id}`}
+              href={`https://www.flickr.com/photos/aymericf/${photo.id}`}
+              target="_blank"
+            >
+              <motion.img
+                key={photo.id}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                className="flickrImage"
+                src={photo.path}
+              />
+            </a>
+          );
+        })}
+      </motion.div>
       {/* LOADING STATE */}
       {!loading && (
         <motion.div className="loader">
@@ -42,74 +144,7 @@ function Projects() {
           />
         </motion.div>
       )}
-      {/* IMAGES */}
-      {loading && (
-        <motion.div className="photoBanner">
-          <motion.h1>
-            A small collection of my photos. <br /> Follow me on{" "}
-            {/* Link to Flickr Account */}
-            <span>
-              <motion.a
-                className="flickr-anchor"
-                initial={false}
-                animate={{
-                  background:
-                    "linear-gradient(98deg, rgba(82,160,218,1) 0%, rgba(82,160,218,1) 50%, rgba(82,160,218,1) 100%)",
-                  WebkitBackgroundClip: "text",
-                  WebkitTextFillColor: "transparent",
-                }}
-                href="https://www.flickr.com/photos/aymericf/"
-                rel="noreferrer noopener"
-                target="_blank"
-                whileHover={{
-                  background: [
-                    "linear-gradient(98deg, rgba(252,176,69,1) 0%, rgba(131,58,180,1) 50%, rgba(253,29,29,1) 100%)",
-                    "linear-gradient(98deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%)",
-
-                    "linear-gradient(98deg, rgba(253,29,29,1) 0%, rgba(252,176,69,1) 50%, rgba(131,58,180,1) 100%)",
-
-                    "linear-gradient(98deg, rgba(252,176,69,1) 0%, rgba(131,58,180,1) 50%, rgba(253,29,29,1) 100%)",
-                  ],
-                  transition: { loop: Infinity, duration: 2, type: "spring" },
-                }}
-              >
-                Flickr
-              </motion.a>
-            </span>{" "}
-            or{" "}
-            <span>
-              <motion.a
-                className="flickr-anchor"
-                initial={false}
-                animate={{
-                  background:
-                    "linear-gradient(98deg, rgba(82,160,218,1) 0%, rgba(82,160,218,1) 50%, rgba(82,160,218,1) 100%)",
-                  WebkitBackgroundClip: "text",
-                  WebkitTextFillColor: "transparent",
-                }}
-                href="https://www.instagram.com/aymericjlf/"
-                rel="noreferrer noopener"
-                target="_blank"
-                whileHover={{
-                  background: [
-                    "linear-gradient(98deg, rgba(252,176,69,1) 0%, rgba(131,58,180,1) 50%, rgba(253,29,29,1) 100%)",
-                    "linear-gradient(98deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%)",
-
-                    "linear-gradient(98deg, rgba(253,29,29,1) 0%, rgba(252,176,69,1) 50%, rgba(131,58,180,1) 100%)",
-
-                    "linear-gradient(98deg, rgba(252,176,69,1) 0%, rgba(131,58,180,1) 50%, rgba(253,29,29,1) 100%)",
-                  ],
-                  transition: { loop: Infinity, duration: 2, type: "spring" },
-                }}
-              >
-                Instagram
-              </motion.a>
-            </span>
-            .
-          </motion.h1>
-        </motion.div>
-      )}
-      <motion.div className="imageContainer">{loading && photos}</motion.div>
+      <button onClick={increase}> See More</button>
     </motion.div>
   );
 }
