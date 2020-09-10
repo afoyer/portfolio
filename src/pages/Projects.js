1584c44314bdb6025abf52f23f4faf8d73f302cb
afoyer <lemimic213@gmail.com>
2020-09-10 17:32:27 -0500

    removed console.log

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -17,7 +17,6 @@ function Projects() {
       observer.current = new IntersectionObserver((entries) => {
         if (entries[0].isIntersecting && hasMore) {
           increase();
-          console.log(pagenumber);
         }
       });
       if (node) observer.current.observe(node);
